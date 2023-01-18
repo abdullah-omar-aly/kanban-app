05bb8dfb78d961721a44f241fd4a83e413be7c78
abdullah-omar-aly <abdullah.omar.aly@gmail.com>
2023-01-18 09:29:50 +0200

    initial commit

---
+++ src/js/Board.js
@@ -0,0 +1,34 @@
+export class Board {
+    clean() {
+        const draggingContainers = document.querySelectorAll('.dragging-container')
+        draggingContainers.forEach(container => container.innerHTML = "")
+    }
+
+    renderTasks(tasks) {
+        const draggingContainers = document.querySelectorAll('.dragging-container')
+        tasks.forEach(task => {
+            draggingContainers.forEach(container => {
+                if (container.getAttribute('data-status') === task.status) {
+                    container.innerHTML += `
+                            <div 
+                                    class="task-card draggable"
+                                    draggable="true" 
+                                    data-id="${task.id}"
+                                    ondragstart="dragCardStart(event)"
+                                    ondragend="dragCardEnd(event)"  
+                                    data-bs-target="#task-details-modal"
+                                    data-bs-toggle="modal"    
+                                >
+                                    <h5>${task.title}</h5>
+                                    <p>${task.description}</p>
+                                </div>
+            
+                    `
+                }
+            })
+        })
+    }
+
+}
+
+
---
+++ src/js/Task.js
@@ -0,0 +1,44 @@
+export class Task {
+
+    create(title , description) {
+
+        const id = Date.now().toString()
+        const newTask = {
+            id,
+            title ,
+            description ,
+            status: 'todo'
+        }
+
+        localStorage.setItem('tasks', JSON.stringify([...this.getAllTasks(), newTask]))
+
+    }
+    getAllTasks () {
+        return JSON.parse(localStorage.getItem('tasks'))
+    }
+
+    getSingleTask(taskId) {
+        return this.getAllTasks().filter(task => task.id === taskId)[0]
+    }
+
+    delete (taskId) {
+        const tasks = this.getAllTasks().filter(task => task.id !== taskId) 
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
+
+    // update({
+    //     taskId , title , description
+    // }) {
+    //     const task = this.getSingleTask(taskId)
+    //     console.log(task)
+    //     task.title = title
+    //     task.description = description
+
+    //     this.delete(taskId)
+    //     localStorage.setItem('tasks', JSON.stringify([...this.getAllTasks(), task]))
+    // }
+}
+
+  
+
+
